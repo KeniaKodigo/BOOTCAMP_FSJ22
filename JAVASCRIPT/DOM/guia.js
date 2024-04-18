de7a621a5a0236026b04d50da929ea60c05d124e
= <121195514+KeniaKodigo@users.noreply.github.com>
2024-04-17 20:14:38 -0600

    ejercicios con DOM

---
+++ JAVASCRIPT/DOM/guia.js
@@ -0,0 +1,14 @@
+/**
+ * - Dada una secuencia de numeros cuantos numeros son multiplos de 15
+ * - Crear la simulacion de una calculadora, el usuario debe ingresar 2 numeros y hacer el tipo de operacion que corresponda
+ */
+
+[1,2,3,4,5,10]
+
+if(numero % 15 === 0){
+    //es multiple de 15
+}
+
+const paragraph = 'The quick brown fox jumps over the lazy dog. It barked.';
+const regex = /[0-9]/g;
+const found = paragraph.match(regex);
